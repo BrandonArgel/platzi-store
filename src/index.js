34b7d6958a24d4899e81c4dcd16a2c28a8122116
BrandonArgel <brandargel@gmail.com>
2022-02-25 11:35:00 -0600

    :wastebasket: [REMOVE] Code of other branches

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Template from '@templates/Template.js';
-import "@styles/main.css";
-import "@styles/styles.scss";
-
-(async function App() {
-  const main = null || document.getElementById('main');
-  main.innerHTML = await Template();
-})();
